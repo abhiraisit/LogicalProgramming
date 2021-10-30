e0d664815da1ea961b9005d516646a414849ac1f
abhiraisit <abhiraisit@gmail.com>
2021-10-30 13:03:39 +0530

    test

---
+++ DivideExample.java
@@ -0,0 +1,27 @@
+class DivideExample {
+
+    public static void main(String[] args) {
+        int n = 14;
+
+        if(n/5 == 0){
+            System.out.println("Divisible by 5");
+        } else {
+            System.out.println("Not Divisible by 5");
+        }
+
+        int p = main();
+        System.out.println(p);
+    }
+
+    public static int main() {
+        int n = 10;
+
+        if(n/5 == 0){
+            System.out.println("Divisible by 5");
+        } else {
+            System.out.println("Not Divisible by 5");
+        }
+        return n;
+    }
+
+}
